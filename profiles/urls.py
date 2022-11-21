e1bdfe8a4831b3b78c0bf662858d08f322afe10f
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-11-21 16:04:40 +0100

    implementation of login and register routes

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,4 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from profiles import views
+
+base_router = DefaultRouter()
+base_router.register(
+    'profiles', views.UserProfileViewSet)
 
 
-urlpatterns = []
+urlpatterns = [
+    path(r'', include(base_router.urls)),
+    path('login', views.UserLoginAPIView.as_view())
+]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.filters import SearchFilter
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 # Create your views here.
+from profiles import serializers, models, permissions
+
+
+class UserProfileViewSet(ModelViewSet):
+    '''APi endpoints for the userprofile'''
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    filter_backends = (SearchFilter,)
+    search_fields = ('email', 'first_name', 'last_name')
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+
+
+class UserLoginAPIView(ObtainAuthToken):
+    '''Handles user login token generation'''
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+# class UserProfileView(ViewSet):
+#     '''API endpoints for the userprofile'''
+#     # list, create, retrieve, update, partial_update, destroy
+#     serializer_class = serializers.UserProfileSerializer
+#     model = models.UserProfile
+
+#     def list(self, request):
+#         serializer = self.serializer_class(
+#                   self.model.objects.all(), many=True)
+#         return Response(serializer.data)
+
+#     def retrieve(self, request, pk=None):
+#         try:
+#             user = self.model.objects.get(pk=pk)
+#             serializer = self.serializer_class(user)
+#             return Response(serializer.data)
+#         except Exception as e:
+#             return Response({}, status=status.HTTP_404_NOT_FOUND)
+
+#     def create(self, request):
+#         serializer = self.serializer_class(data=request.data)
+#         if serializer.is_valid():
+#             serializer.create(serializer.validated_data)
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response(
+#                 serializer.errors,
+#                 status=status.HTTP_400_BAD_REQUEST
+#             )

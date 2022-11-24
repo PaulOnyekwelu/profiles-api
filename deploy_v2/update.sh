a54507b513084658e4fbb104066956d424688718
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-11-24 04:55:47 +0100

    updated deployment files

--- deploy_v2/update.sh
+++ deploy_v2/update.sh
@@ -4,6 +4,10 @@ set -e
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
+cd PROJECT_BASE_PATH
+pipenv shell
+pipenv install
+
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
